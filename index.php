6b02f70efb674db27ac4bdac938823fd72c9c32c
pravina0911 <144002346+pravina0911@users.noreply.github.com>
2023-09-05 16:21:30 +0800

    pravina

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 <body>
 <form action="papar.php" method="post">
     <label for="name">Ceritakan tentang diri anda:</label>
-    <input type="text" id="name" name="description">
+    <textarea id="name" name="description"> </textarea>
     <input type="submit" value="Simpan">
 </form>
 </body>
